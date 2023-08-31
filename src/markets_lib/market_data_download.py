89670dda8e5afed5319eacdf4eb0cf0899a749b1
Anupam Basu <prime.lens@gmail.com>
2023-08-31 03:38:49 -0500

    New API query

--- src/markets_lib/market_data_download.py
+++ src/markets_lib/market_data_download.py
@@ -3,25 +3,23 @@
 
 """
 """
-import argparse, datetime, logging, os, re, signal, sys, time
+import argparse, logging, os, re, signal, sys, time
 import pandas as pd
 
 import alphavantage as av
-import timeseries as ts
-
 
 # Parser for command line parameters
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-q', '--queries_per_day', default=450, help='Maximum number of queries allowed per day', type=int)
+parser.add_argument('-pq', '--pause_between_queries', default=-1, help='If this is set, we run -q queries and stop', type=int)
 parser.add_argument('-tl', '--ticker_list', default='../../data/sac_tickers_largecap.csv,../../data/sac_tickers_midcap.csv,../../data/av_listed_tickers_etfs.csv', help='Comma separated list of CSV stock tickers where first column is ticker (header row is ignored).', type=str)
 parser.add_argument('-o', '--output_directory', default='~/Desktop/Server/market_data/', help='Output directory', type=str)
 parser.add_argument('-lf', '--log_file', default='_logfile.log', help='Log file. Written to output directory/API_call/.', type=str)
+parser.add_argument('-k', '--alphavantage_api_key', default='apikey.cfg', help='The file with the API key used to retrieve data from alphavantage', type=str)
 parser.add_argument('-api', '--alphavantage_api_call', default='TIME_SERIES_DAILY', help='The API call used to retrieve data from alphavantage', type=str)
 parser.add_argument('-f', '--first_ticker', default=None, help='The first ticker in the list to start with.', type=str)
-parser.add_argument('-i', '--interval', default='5min', help='For intraday data: api TIME_SERIES_INTRADAY_EXTENDED', type=str)
-parser.add_argument('-ipm', '--ignore_prepost_market', default=True, help='For intraday data: api TIME_SERIES_INTRADAY_EXTENDED', type=bool)
-parser.add_argument('-mo', '--mkt_open', default='9:30', help='For intraday data: api TIME_SERIES_INTRADAY_EXTENDED', type=str)
-parser.add_argument('-mc', '--mkt_close', default='16:00', help='For intraday data: api TIME_SERIES_INTRADAY_EXTENDED', type=str)
+parser.add_argument('-p', '--parameters', default=[], help='Key=Value (no space) pairs to be passed to API call.', metavar='KEY=VALUE', nargs='+')
+
 args=parser.parse_args()
 
 # Print out a summary of parameters
@@ -30,19 +28,29 @@ for arg in vars(args):
     print('{:>40} : {}'.format(arg, vars(args)[arg]))
 
 # Parse CLI srgs into variables
-pause_between_queries = int((24*60*60) / args.queries_per_day)
+if args.pause_between_queries < 0:
+    pause_between_queries = int((24*60*60) / args.queries_per_day)
+    max_queries = None
+else:
+    max_queries = args.queries_per_day
+    pause_between_queries = args.pause_between_queries
 ticker_list = [os.path.expanduser(p) for p in args.ticker_list.split(',')]
 output_directory = os.path.expanduser(args.output_directory)
+av_api_key = args.alphavantage_api_key
 av_api_call = args.alphavantage_api_call
 log_file = os.path.join(output_directory, av_api_call, args.log_file)
 first_ticker = args.first_ticker
 current_ticker = None
-interval = args.interval
-ignore_prepost_market = args.ignore_prepost_market
-mkt_open = tuple(int(x) for x in args.mkt_open.split(':'))
-mkt_open = datetime.time(mkt_open[0], mkt_open[1])
-mkt_close = tuple(int(x) for x in args.mkt_close.split(':'))
-mkt_close = datetime.time(mkt_close[0], mkt_close[1])
+# Parse parameters into integer, float, or string
+parameters = {p.split('=')[0]:p.split('=')[1] for p in args.parameters}
+for k in parameters:
+    try:
+        parameters[k] = int(parameters[k])
+    except ValueError:
+        try:
+            parameters[k] = float(parameters[k])
+        except ValueError:
+            pass
 
 # Set up logging
 for handler in logging.root.handlers[:]:
@@ -55,7 +63,6 @@ sh.setLevel(logging.INFO)
 fmt = '%(asctime)s %(message)s'
 dfmt = '%y-%m-%d  %H:%M:%S'
 logging.basicConfig(handlers=(fh, sh), format=fmt, datefmt=dfmt, level=logging.INFO)
-
 
 ##############################################################################
 def parse_ticker_list(ticker_list):
@@ -141,7 +148,7 @@ def get_path(ticker):
     """
     return os.path.join(output_directory,av_api_call,'{}.csv'.format(ticker))
 
-def get_ticker_data_from_api(ticker, parameters={'outputsize':'full'}):
+def get_ticker_data_from_api(ticker, av_api_call, api_key, parameters={}):
     """
     Queries the API and returns a Dataframe, Dictionary or None
 
@@ -156,32 +163,20 @@ def get_ticker_data_from_api(ticker, parameters={'outputsize':'full'}):
         Parsed data - Dataframe for timeseries, Dictionary for key-value 
         fundamental data, none if failed.
     """
-    av_response = av.call_api(symbol=ticker, function=av_api_call, parameters=parameters)
-    parsed_response, columns = av.parse_response(av_response, function=av_api_call, symbol=ticker)
-    if parsed_response:
-        if isinstance(parsed_response, list):
-            data_frame = ts.timeseries_df(parsed_response, columns)
-            if av_api_call == 'TIME_SERIES_INTRADAY_EXTENDED' and ignore_prepost_market == True:
-                mask = data_frame['Date'].apply(lambda d: d.time() < mkt_open or d.time() > mkt_close)
-                data_frame.drop(data_frame[mask].index)
-            data_frame = data_frame.sort_values('Date')            
-            return data_frame
-        else:
-            dictionary = parsed_response
-            return dictionary
-    else:
-        return None
+    av_response = av.call_api(ticker, av_api_call, api_key, parameters=parameters)
+
+    return av_response
     
-def check_overwrite_tickerfile(ticker, ticker_data):
+def check_overwrite_tickerfile(ticker, ticker_df):
     ticker_file = get_path(ticker)
     if os.path.exists(ticker_file):
         logging.info('{:>6}: Reading {}.'.format(ticker, ticker_file))
         # File is either CSV (pandas) or JSON
         if ticker_file.lower().split('.')[-1] == 'csv':
-            ticker_df_old = pd.read_csv(ticker_file, index_col=[0], parse_dates=['Date'])
-            if len(ticker_data) > len(ticker_df_old):
+            ticker_df_old = pd.read_csv(ticker_file)
+            if len(ticker_df) > len(ticker_df_old):
                 logging.info('{:>6}: Updating {}.'.format(ticker, ticker_file))
-                ticker_data.to_csv(ticker_file, float_format='%.2f')
+                ticker_df.to_csv(ticker_file, float_format='%.2f')
             else:
                 logging.info('{:>6}: {} already up to date.'.format(ticker, ticker_file))
         else:
@@ -189,7 +184,7 @@ def check_overwrite_tickerfile(ticker, ticker_data):
             pass
     else:
         logging.info('{:>6}: Creating file {}.'.format(ticker, ticker_file))
-        ticker_data.to_csv(ticker_file, float_format='%.2f')
+        ticker_df.to_csv(ticker_file, float_format='%.2f')
     
 
 def pause(ticker, n, width=100, segments=100):
@@ -224,9 +219,17 @@ signal.signal(signal.SIGINT, exit_gracefully) # Catch keyboard interrupt
 tickers = parse_ticker_list(ticker_list)      # Get tickers
 first_ticker = parse_log_last_ticker()         # Get last / first ticker
 
+api_key = av.get_api_key(config_file=av_api_key)
+n_queries = 0
+
 # Loop until keyboard interrupt
 while True:
     for n, ticker in enumerate(tickers):
+        if max_queries != None and first_ticker == None:
+            logging.info('{} of {} daily queries.'.format(n_queries, max_queries))
+            if n_queries >= max_queries:
+                logging.error('{}: Quitting before retrieving data - max queries reached.'.format(current_ticker))
+                break
         # If this is the first instance of the loop then skip forward to the 
         # last ticker that was about to be processed before quitting.
         if first_ticker:
@@ -237,13 +240,11 @@ while True:
         
         current_ticker = ticker
         pause(ticker, n)
-        if av_api_call == 'TIME_SERIES_INTRADAY_EXTENDED':
-            parameters = {'interval':interval, 'adjusted':'true', 'pause_between_queries':pause_between_queries}
-        else:
-            parameters = {'outputsize':'full'}
-        ticker_data = get_ticker_data_from_api(ticker, parameters=parameters)
-        if isinstance(ticker_data, pd.DataFrame):
-            check_overwrite_tickerfile(ticker, ticker_data)
+        ticker_df, nq = get_ticker_data_from_api(ticker, av_api_call, api_key, parameters=parameters)
+
+        n_queries += nq
+        if isinstance(ticker_df, pd.DataFrame):
+            check_overwrite_tickerfile(ticker, ticker_df)
         else:
             continue
                     
