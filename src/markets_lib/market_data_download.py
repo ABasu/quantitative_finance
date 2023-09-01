bf5c5c143aeff1e7706b4b44259b0cf949ba6649
Anupam Basu <prime.lens@gmail.com>
2023-09-01 05:12:38 -0500

    Added non US exchanges

--- src/markets_lib/market_data_download.py
+++ src/markets_lib/market_data_download.py
@@ -22,10 +22,11 @@ import alphavantage as av
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description = help_string)
 parser.add_argument('-q',  '--queries_per_day', default=100, help='Maximum number of queries allowed per day', type=int)
 parser.add_argument('-pq', '--pause_between_queries', default=-1, help='If this is set, we run -q queries and stop', type=int)
-parser.add_argument('-tl', '--ticker_list', default='../../data/sac_tickers_largecap.csv,../../data/sac_tickers_midcap.csv,../../data/av_listed_tickers_etfs.csv', help='Comma separated list of CSV stock tickers where first column is ticker (header row is ignored).', type=str)
+parser.add_argument('-tl', '--ticker_list', default='../../data/sac_tickers_largecap.csv,../../data/sac_tickers_midcap.csv', help='Comma separated list of CSV stock tickers where first column is ticker (header row is ignored).', type=str)
+parser.add_argument('-e',  '--exchange', default=None, help='Exchange code. Default is NYSE (i.e. empty)', type=str)
 parser.add_argument('-o',  '--output_directory', default='~/Desktop/Server/market_data/', help='Output directory', type=str)
 parser.add_argument('-k',  '--alphavantage_api_key', default='apikey.cfg', help='The file with the API key used to retrieve data from alphavantage', type=str)
-parser.add_argument('-af',  '--alphavantage_api_function', default='TIME_SERIES_DAILY', help='The API call used to retrieve data from alphavantage', type=str)
+parser.add_argument('-af', '--alphavantage_api_function', default='TIME_SERIES_DAILY', help='The API call used to retrieve data from alphavantage', type=str)
 parser.add_argument('-l',  '--log_file', default='_logfile.log', help='Log file. Written to output directory/API_call/.', type=str)
 parser.add_argument('-f',  '--first_ticker', default=None, help='The first ticker in the list to start with.', type=str)
 parser.add_argument('-p',  '--parameters', default=[], help='Key=Value (no space) pairs to be passed to API call.', metavar='KEY=VALUE', nargs='+')
@@ -46,10 +47,11 @@ else:
     max_queries = args.queries_per_day
     pause_between_queries = args.pause_between_queries
 ticker_list = [os.path.expanduser(p) for p in args.ticker_list.split(',')]
+exchange = args.exchange 
 output_directory = os.path.expanduser(args.output_directory)
 av_api_key = args.alphavantage_api_key
 av_api_function = args.alphavantage_api_function
-log_file = os.path.join(output_directory, av_api_function, args.log_file)
+log_file = args.log_file
 first_ticker = args.first_ticker
 current_ticker = None
 # Parse parameters into integer, float, or string
@@ -63,20 +65,8 @@ for k in parameters:
         except ValueError:
             pass
 
-# Set up logging
-for handler in logging.root.handlers[:]:
-    logging.root.removeHandler(handler)
-
-fh = logging.FileHandler(log_file)
-fh.setLevel(logging.ERROR)
-sh = logging.StreamHandler(sys.stderr)
-sh.setLevel(logging.INFO)
-fmt = '%(asctime)s %(message)s'
-dfmt = '%y-%m-%d  %H:%M:%S'
-logging.basicConfig(handlers=(fh, sh), format=fmt, datefmt=dfmt, level=logging.INFO)
-
 ##############################################################################
-def check_overwrite_tickerfile(ticker, ticker_df):
+def check_overwrite_tickerfile(ticker, api_function, exchange, ticker_df):
     """
     For a given dataframe, checks to see if the data already exists on disk. If
     so, loads the file and compares to see if there are new lines in the dataframe.
@@ -94,7 +84,7 @@ def check_overwrite_tickerfile(ticker, ticker_df):
     None.
 
     """
-    ticker_file = get_path(ticker)
+    ticker_file = get_path(ticker, api_function, exchange)
     if os.path.exists(ticker_file):
         logging.info('{:>6}: Reading {}.'.format(ticker, ticker_file))
         # File is either CSV (pandas) or JSON
@@ -112,7 +102,8 @@ def check_overwrite_tickerfile(ticker, ticker_df):
         logging.info('{:>6}: Creating file {}.'.format(ticker, ticker_file))
         ticker_df.to_csv(ticker_file, float_format='%.2f')
     
-def exit_gracefully(sig, frame):
+##############################################################################
+def exit_gracefully():
     """
     Logs the ticker about to be retreived, but incomplete, when exiting.
 
@@ -126,10 +117,32 @@ def exit_gracefully(sig, frame):
     None.
 
     """
-    logging.error('{}: Quitting before retrieving data'.format(current_ticker))
+    logging.error('{}: Quitting before retrieving data'.format(ticker))
     sys.exit(0)
 
-def get_path(ticker):
+##############################################################################
+def get_dir(api_function, exchange):
+    """
+    Return the directory for the current call. e.g. TIME_SERIES_DAILY or 
+    TIME_SERIES_DAILY.BSE etc
+
+    Parameters
+    ----------
+    ticker : str
+        Ticker.
+    exchange : str
+        Exchange code.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+    return '.'.join([api_function, exchange]) if exchange != None else api_function
+
+##############################################################################
+def get_path(ticker, api_function, exchange):
     """
     Figures out the path for the output file based on the current api call and
     the ticker. 
@@ -146,9 +159,10 @@ def get_path(ticker):
         File path.
 
     """
-    return os.path.join(output_directory,av_api_function,'{}.csv'.format(ticker))
+    return os.path.join(output_directory, get_dir(api_function, exchange),'{}.csv'.format(ticker))
 
-def parse_log_last_ticker():
+##############################################################################
+def parse_log_last_ticker(log_file):
     """
     At startup, this function is called to find out what the last ticker was 
     that was being retrieved when last shut down. It reads to logfile to see
@@ -164,10 +178,12 @@ def parse_log_last_ticker():
         log_last_line = open(log_file, 'r').readlines()[-1]
     except:
         log_last_line = ''
-    match_ticker = re.search('\d\d-\d\d-\d\d\s+\d\d:\d\d:\d\d\s+([A-Z]+): Quitting', log_last_line)
+    match_ticker = re.search('\d\d-\d\d-\d\d\s+\d\d:\d\d:\d\d\s+([A-Z\d]+): Quitting', log_last_line)
     last_ticker = match_ticker.group(1) if match_ticker != None else None
+
     return last_ticker
 
+##############################################################################
 def parse_ticker_list(ticker_list):
     """
     Read a list of CSV files and parse out the first column as a list of ticker
@@ -186,6 +202,8 @@ def parse_ticker_list(ticker_list):
     tickers_all = []
     for tl in ticker_list:
         tickers = open(tl, 'r').readlines()
+        # remove commented lines
+        tickers = [t for t in tickers if t[0]!='#']
         tickers = [l.split(',')[0] for l in tickers[1:]]
         # Remove special classes of tickers that have $ signs
         tickers = [l for l in tickers if not re.search('\$', l)]
@@ -195,6 +213,7 @@ def parse_ticker_list(ticker_list):
         
     return tickers_all
 
+##############################################################################
 def pause(ticker, n, width=100, segments=100):
     """
     Waits for pause_between_queries while updating the screen with a timer bar
@@ -222,38 +241,56 @@ def pause(ticker, n, width=100, segments=100):
     print(' '*(width+50), end='\r')
     
 ##############################################################################
-signal.signal(signal.SIGINT, exit_gracefully) # Catch keyboard interrupt
-tickers = parse_ticker_list(ticker_list)      # Get tickers
-if first_ticker == None:
-    first_ticker = parse_log_last_ticker()    # Get last / first ticker
 
-api_key = av.get_api_key(config_file=av_api_key)
-n_queries = 0
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, exit_gracefully) # Catch keyboard interrupt
 
-# Loop until keyboard interrupt
-while True:
-    for n, ticker in enumerate(tickers):
-        if max_queries != None and first_ticker == None:
-            logging.info('{} of {} daily queries done.'.format(n_queries, max_queries))
-            if n_queries >= max_queries:
-                logging.error('{}: Quitting before retrieving data - max queries reached.'.format(current_ticker))
-                break
-        # If this is the first instance of the loop then skip forward to the 
-        # last ticker that was about to be processed before quitting.
-        if first_ticker:
-            if ticker != first_ticker:
-                continue                      # Ignore until we see last ticker
-            else:
-                first_ticker = None            # Delete for future loops
-        
-        current_ticker = ticker
-        pause(ticker, n)
-        ticker_df, nq = av.call_api(ticker, av_api_function, api_key, parameters=parameters)
+    # Set up logging
+    log_file = os.path.join(output_directory, get_dir(av_api_function, exchange), log_file)
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)    
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.ERROR)
+    sh = logging.StreamHandler(sys.stderr)
+    sh.setLevel(logging.INFO)
+    fmt = '%(asctime)s %(message)s'
+    dfmt = '%y-%m-%d  %H:%M:%S'
+    logging.basicConfig(handlers=(fh, sh), format=fmt, datefmt=dfmt, level=logging.INFO)
 
-        n_queries += nq
-        if isinstance(ticker_df, pd.DataFrame):
-            check_overwrite_tickerfile(ticker, ticker_df)
-        else:
-            continue
-                    
+    tickers = parse_ticker_list(ticker_list)      # Get tickers
+    if first_ticker == None:
+        first_ticker = parse_log_last_ticker(log_file)    # Get last / first ticker
     
+    api_key = av.get_api_key(config_file=av_api_key)
+    n_queries = 0
+
+    # Loop until keyboard interrupt
+    while True:
+        # Loop thru complete list of tickers
+        for n, ticker in enumerate(tickers):
+            # If this is the first instance of the loop then skip forward to the 
+            # last ticker that was about to be processed before quitting.
+            if first_ticker:
+                if ticker != first_ticker:
+                    continue                      # Ignore until we see last ticker
+                else:
+                    first_ticker = None            # Delete for future loops
+
+            # We don't go over max_queries if it's set
+            if max_queries != None:
+                logging.info('{} of {} daily queries done.'.format(n_queries, max_queries))
+                if n_queries >= max_queries:
+                    logging.error('{}: Quitting before retrieving data - max queries reached.'.format(ticker))
+                    break
+            
+            pause(ticker, n)
+            ticker_data, nq = av.call_api(ticker, av_api_function, api_key, \
+                                exchange=exchange, parameters=parameters, normalize_column_headings=True)
+    
+            n_queries += nq
+            if isinstance(ticker_data, pd.DataFrame):
+                check_overwrite_tickerfile(ticker, av_api_function, exchange, ticker_data)
+            else:
+                continue
+                        
+        
